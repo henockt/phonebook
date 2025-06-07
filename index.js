4141332b2df5c52e71dbc863ed81fa36b260c9ce
henockt <43278491+henockt@users.noreply.github.com>
2025-06-07 11:03:02 +0300

    up to 3.14

--- index.js
+++ index.js
@@ -1,61 +1,53 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
 // logging
-morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body) 
+})
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 app.get('/info', (request, response) => {
-    response.writeHead(200, { 'Content-type': 'text/html' })
+    Person.find({}).then(people => {
+        response.writeHead(200, { 'Content-type': 'text/html' })
 
-    response.write(`<p>Phonebook has info for ${persons.length}</p>`)
-    response.write(`<p>${new Date().toString()}</p>`)
-    response.end()
+        response.write(`<p>Phonebook has info for ${people.length}</p>`)
+        response.write(`<p>${new Date().toString()}</p>`)
+        response.end()
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find(p => p.id === id)
-
-    if (!person) {
-        response.status(404).end()
-    } else {
-        response.json(person)
-    }
+    Person
+        .findById(id)
+        .then(person => {
+            if (!person) {
+                response.status(404).end()
+            } else {
+                response.json(person)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            response.status(400).send({error: 'malformatted id'})
+        })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -67,26 +59,26 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    // if (persons.find(p => p.name === body.name)) {
+    //     return response.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: (Math.floor(Math.random() * (100 + 1 - 1) + 1)).toString() // random number b/n [1, 100]
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+    // persons = persons.filter(p => p.id !== id)
+    // response.status(204).end()
 })
 
 const unknownEndpoint = (request, response) => {
@@ -94,7 +86,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     console.log(`http://localhost:${PORT}`)
